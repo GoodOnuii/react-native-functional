eb6fa24c59bdfa97f2e2b4152a3c72debb43ff90
Hana <momenthana@kakao.com>
2024-02-07 23:00:53 +0900

    feat: react-native-functional injected webview

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,25 @@
-import * as React from 'react';
+import React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-functional';
+import { StyleSheet, View } from 'react-native';
+import Functional from 'react-native-functional';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Functional
+        roomId={'id'}
+        accessToken={'token'}
+        onEvent={(event) => {
+          console.log('event: ', event);
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+    width: '100%',
+    height: '100%',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,48 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { registerGlobals } from '@livekit/react-native';
+import { RoomPage } from './livekit';
+import React, { useRef } from 'react';
+
+registerGlobals();
+
+interface FunctionalProps {
+  roomId: string;
+  accessToken: string;
+  onEvent?: (event: any) => void;
 }
+
+export default function Functional(props: FunctionalProps) {
+  const { roomId, accessToken, onEvent } = props;
+  const webViewRef = useRef(null);
+
+  const onMessage = (event: { nativeEvent: { data: string } }) => {
+    //receive message from the web page. working here until here
+    const data = JSON.parse(event.nativeEvent.data);
+
+    if (onEvent) onEvent(data);
+  };
+
+  return (
+    <View style={styles.container}>
+      <WebView
+        ref={webViewRef}
+        style={styles.container}
+        originWhitelist={['*']}
+        source={{
+          uri: `https://draw.seoltab.com/whiteboard?room_id=${roomId}&access_token=${accessToken}`,
+        }}
+        allowsInlineMediaPlayback={true}
+        onMessage={onMessage}
+      />
+      <RoomPage url="url" token="token" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+});
