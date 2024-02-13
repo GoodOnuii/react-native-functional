2dd490941a92539cbc9958cab1b5115c6d7115d1
Hana <momenthana@kakao.com>
2024-02-13 12:01:36 +0900

    fix: v5 which is not compatible with react-native-webrtc

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,6 +3,7 @@ const escape = require('escape-string-regexp');
 const { getDefaultConfig } = require('@expo/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const pak = require('../package.json');
+const resolveFrom = require('resolve-from');
 
 const root = path.resolve(__dirname, '..');
 const modules = Object.keys({ ...pak.peerDependencies });
@@ -37,6 +38,29 @@ const config = {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
+
+    resolveRequest: (context, moduleName, platform) => {
+      if (
+        // If the bundle is resolving "event-target-shim" from a module that is part of "react-native-webrtc".
+        moduleName.startsWith('event-target-shim') &&
+        context.originModulePath.includes('react-native-webrtc')
+      ) {
+        // Resolve event-target-shim relative to the react-native-webrtc package to use v6.
+        // React Native requires v5 which is not compatible with react-native-webrtc.
+        const eventTargetShimPath = resolveFrom(
+          context.originModulePath,
+          moduleName
+        );
+
+        return {
+          filePath: eventTargetShimPath,
+          type: 'sourceFile',
+        };
+      }
+
+      // Ensure you call the default resolver.
+      return context.resolveRequest(context, moduleName, platform);
+    },
   },
 };
 
